fae42c9dc427d709b5d36ca1c4a8866af6e82c3d
Joe <hutcheson2001@yahoo.com>
2022-04-09 17:49:48 -0400

    commit tree bd57ab4bf04d2a18e7fdc862950fff967852a78e

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
-def oxford_comma(array)
+require "pry"
 
+def oxford_comma(array)
+  if array.length == 1 
+    array.join
+  elsif array.length == 2 
+    array.insert(1,"and").join(" ")
+  else 
+    last_word = array.pop
+    array << "and "
+    joined_array = array.join(", ")
+    joined_array + last_word
+  end
+  # binding.pry
 end
